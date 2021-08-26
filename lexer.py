244435d5e75d512f29e5bd86b51afbfcf2100355
yatharthagoenka <goenkayathartha2002@gmail.com>
2021-08-26 13:03:54 +0530

    Bug Fix: BuiltIn Exec

--- lexer.py
+++ lexer.py
@@ -1703,10 +1703,10 @@ class BuiltInFunction(BaseFunction):
         method = getattr(self, method_name, self.no_visit_method)
 
         res.register(self.check_and_populate_args(method.arg_names, args, exec_ctx))
-        if res.should_return(): res
+        if res.should_return(): return res
 
         return_value = res.register(method(exec_ctx))
-        if res.should_return(): res
+        if res.should_return(): return res
         
         return res.success(return_value)
 
@@ -1889,7 +1889,7 @@ class BuiltInFunction(BaseFunction):
         if error:
             return RTResult().failure(RTError(
                 self.pos_start, self.pos_end,
-                f"Failed to load script \"{fn}\"\n" + error.as_string(),
+                f"Failed to finish executing script \"{fn}\"\n" + error.as_string(),
                 exec_ctx
             ))
         
